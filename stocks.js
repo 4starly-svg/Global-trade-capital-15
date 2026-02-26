168f675ddcd77b9149b1ff18b0078ce4288d42c3
4starly-svg <4starly@gmail.com>
2026-02-27 01:14:46 +0200

    Delete stocks.js

--- stocks.js
+++
@@ -1,7 +0,0 @@
-async function loadStocks(){
-  let res = await fetch("https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=YOUR_API_KEY");
-  let data = await res.json();
-  document.getElementById("aapl").innerText = "$" + data[0].price;
-}
-loadStocks();
-setInterval(loadStocks, 30000);
